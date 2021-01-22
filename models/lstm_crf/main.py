d262b1c852a4e7b3292c743ec6b31c9ad86401ed
Liev Garcia <liev.garcia@gmail.com>
2021-01-22 14:15:35 +0000

    moving shared methods into utils, adding CRF to the main model, saving results and enabling test scoring.

--- models/lstm_crf/main.py
+++ models/lstm_crf/main.py
@@ -14,48 +14,34 @@ import functools
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import LSTM, Embedding, Dense
+from tensorflow.keras.layers import LSTM, Embedding, Dense, Lambda
 from tensorflow.keras.layers import TimeDistributed, SpatialDropout1D, Bidirectional
+from keras_crf import CRF
+
+import csv
+from sklearn.metrics import classification_report
 
 # Logging
 import logging
 Path("{}/results".format(path)).mkdir(exist_ok=True)
-# tf.compat.v1.logging.set_verbosity(logging.DEBUG)
-# handlers = [
-#     logging.FileHandler('results/main.log'),
-#     logging.StreamHandler(sys.stdout)
-# ]
-# logging.getLogger('tensorflow').handlers = handlers
+
 
 sys.path.append(str(Path('.').absolute().parent))
-from models.utils import tags_dictionaries, words_dictionaries
+from models.utils import tags_dictionaries, words_dictionaries, fwords, ftags, parse_fn, generator_fn, save_model
 
-def fwords(name):
-    return str(Path(DATADIR, "{}.sentences.csv".format(name)))
-
-def ftags(name):
-    return str(Path(DATADIR, "{}.labels.csv".format(name)))
-
-def parse_fn(line_words, line_tags):
-    words = np.array([word2idx.get(w, 0) for w in line_words.strip().split()])
-    tags = np.array([tag2idx[t] for t in line_tags.strip().split()])
-    assert len(words) == len(tags), "Words and tags lengths don't match"
-    return words, tags
-
-def generator_fn(words, tags):
-    with Path(words).open('r') as f_words, Path(tags).open('r') as f_tags:
-        for line_words, line_tags in zip(f_words, f_tags):
-            yield parse_fn(line_words, line_tags)
 
 def input_fn(words, tags, params=None, shuffle_and_repeat=False):
     params = params if params is not None else {}
+    output_shapes = ([None], [None])
+    output_types = (tf.int32, tf.int32)
     output_signature = (
         tf.TensorSpec(shape=([None]), dtype=tf.int32),
         tf.TensorSpec(shape=([None]), dtype=tf.int32))
 
     dataset = tf.data.Dataset.from_generator(
-        functools.partial(generator_fn, words, tags),
-        output_signature=output_signature
+        functools.partial(generator_fn, words, tags, word2idx, tag2idx),
+        # output_signature=output_signature # this is awaiting for tf2.4.1 release for CRF compatibility.
+        output_shapes=output_shapes, output_types=output_types
     )
 
     if shuffle_and_repeat:
@@ -63,7 +49,7 @@ def input_fn(words, tags, params=None, shuffle_and_repeat=False):
 
     shapes = (tf.TensorShape([None]),tf.TensorShape([None]))
     dataset = (dataset
-               .padded_batch(batch_size=32,
+               .padded_batch(batch_size=params["batch_size"],
                              padded_shapes=([params["max_len"]], [params["max_len"]]),
                              padding_values=(params['vocab_size']-1,params['pad_index'])
                             )
@@ -72,19 +58,42 @@ def input_fn(words, tags, params=None, shuffle_and_repeat=False):
 
 def model_fn(params):
     input_word = Input(shape=(params["max_len"],))
+    input_mask = Lambda(lambda x: tf.greater(x, 0))(input_word)
     model = Embedding(input_dim=params['vocab_size'], output_dim=params["embeddings_dim"], input_length=params["max_len"])(input_word)
     model = SpatialDropout1D(params["dropout"])(model)
     model = Bidirectional(LSTM(units=params["lstm_size"], return_sequences=True, recurrent_dropout=params["dropout"]))(model)
-    out = TimeDistributed(Dense(params["labels_size"], activation="softmax"))(model)
-    model = Model(input_word, out)
-    model.compile(optimizer="adam",
-                  loss="sparse_categorical_crossentropy",
-                  metrics=["accuracy"])
+    logits = Dense(params["labels_size"], activation=None)(model)
+    crf = CRF(params["labels_size"])
+    outputs = crf(logits, mask=input_mask)
+    model = tf.keras.Model(inputs=input_word, outputs=outputs)
+    model.compile(loss=crf.neg_log_likelihood, metrics=[crf.accuracy], optimizer='adam')
 
     return model
 
-def save_model(model):
-    model.save("{}/results/model".format(path))
+def write_predictions(name, idx2tag, datadir, writedir):
+    y_true, y_pred = [],[]
+    target_names = list(idx2tag.values())
+
+    with open('{}/results/{}.preds.csv'.format(writedir, name), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        golds_gen = generator_fn(fwords(datadir, name), ftags(datadir, name), word2idx, tag2idx)
+        dataset = functools.partial(input_fn, fwords(datadir, name), ftags(datadir, name), params, shuffle_and_repeat=False)()
+        preds_gen = model.predict(dataset)
+        for golds, preds in zip(golds_gen, preds_gen):
+
+            (words, tags) = golds
+            preds = np.argmax(preds, axis=-1)[:len(words)]
+            for word, tag, tag_pred in zip(words, tags, preds):
+                y_true.append(idx2tag[tag])
+                y_pred.append(idx2tag[tag_pred])
+                writer.writerow([idx2word[word], idx2tag[tag], idx2tag[tag_pred]])
+    return y_true, y_pred
+
+def write_classification_report(datadir, y_true, y_pred, name):
+    with open("{}/{}.metric-report.txt".format(datadir, name), "w") as f:
+        report = classification_report(y_true, y_pred)
+        f.write(report)
+
 
 if __name__ == '__main__':
     usage = """
@@ -138,12 +147,18 @@ if __name__ == '__main__':
         json.dump(params, f, indent=4, sort_keys=True)
 
     if cmd=="train":
-        dataset = functools.partial(input_fn, fwords('train'), ftags('train'), params, shuffle_and_repeat=True)()
-        valid_dataset = functools.partial(input_fn, fwords('test'), ftags('test'), params, shuffle_and_repeat=True)()
+        dataset = functools.partial(input_fn, fwords(DATADIR, 'train'), ftags(DATADIR, 'train'), params, shuffle_and_repeat=True)()
+        valid_dataset = functools.partial(input_fn, fwords(DATADIR, 'test'), ftags(DATADIR, 'test'), params, shuffle_and_repeat=True)()
         model = model_fn(params)
-        model.fit(dataset, validation_data=valid_dataset, epochs=1)
-        save_model(model)
-    elif cmd=="test":
-        model=tf.keras.models.load_model("{}/results/model".format(path))
-        valid_dataset = functools.partial(input_fn, fwords('test'), ftags('test'), params, shuffle_and_repeat=True)()
+        model.fit(dataset, validation_data=valid_dataset, epochs=params["epochs"])
+        save_model(model, path)
         predictions = model.predict(valid_dataset)
+        y_true, y_pred = write_predictions('train', idx2tag, DATADIR, path)
+        _ = write_classification_report(path, y_true, y_pred, 'train')
+    elif cmd=="test":
+        model=tf.keras.models.load_model("{}/results/model".format(path), compile=False)
+        model.compile()
+        valid_dataset = functools.partial(input_fn, fwords(DATADIR, 'test'), ftags(DATADIR, 'test'), params, shuffle_and_repeat=False)()
+        predictions = model.predict(valid_dataset)
+        y_true, y_pred = write_predictions('test', idx2tag, DATADIR, path)
+        _ = write_classification_report(path, y_true, y_pred, 'test')
--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import numpy as np
 
 def tags_dictionaries(params):
     with Path(params['tags']).open() as f:
@@ -13,3 +14,23 @@ def words_dictionaries(params):
         word2idx["ENDPAD"]=len(word2idx)
         idx2word = {i: t for t, i in word2idx.items()}
     return word2idx, idx2word, len(word2idx)
+
+def fwords(datadir, name):
+    return str(Path(datadir, "{}.sentences.csv".format(name)))
+
+def ftags(datadir, name):
+    return str(Path(datadir, "{}.labels.csv".format(name)))
+
+def parse_fn(line_words, line_tags, word2idx, tag2idx):
+    words = np.array([word2idx.get(w, 0) for w in line_words.strip().split()])
+    tags = np.array([tag2idx[t] for t in line_tags.strip().split()])
+    assert len(words) == len(tags), "Words and tags lengths don't match"
+    return words, tags
+
+def generator_fn(words, tags, word2idx, tag2idx):
+    with Path(words).open('r') as f_words, Path(tags).open('r') as f_tags:
+        for line_words, line_tags in zip(f_words, f_tags):
+            yield parse_fn(line_words, line_tags, word2idx, tag2idx)
+
+def save_model(model, path):
+    model.save("{}/results/model".format(path))
